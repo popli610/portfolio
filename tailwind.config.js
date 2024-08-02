130093982116dbf0b098ac11159eb839225c4fd3
Nishant Popli <nishantpopli1234@gmail.com>
2024-08-02 16:05:13 +0530

    optimized image for faster loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
       },
       backgroundImage: {
         site: "url('./assets/site-bg.jpg')",
-        about: "url('./assets/about.JPG')",
+        about: "url('./assets/about.webp')",
         services: "url('./assets/services.png')",
       },
     },
